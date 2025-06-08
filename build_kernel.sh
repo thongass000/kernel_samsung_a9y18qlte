9629a3c0c7e5338642bb670e216e2c84e7b63308
James Nguyen <59555509+thongass000@users.noreply.github.com>
2025-06-08 17:14:07 +0000

    Revert "buildscript: fix cloning kernelsu to the wrong path"
    This reverts commit eb7283dd271b07d69e1bfbd8da1cb337562290e2.

--- build_kernel.sh
+++ build_kernel.sh
@@ -51,7 +51,7 @@ if [ "$KSU" == "CONFIG_KSU=y" ]; then
 	cd drivers
 	rm -rf kernelsu &>> $REALLOGGER
 	cd ..
-	git clone https://github.com/riarumoda/KernelSU-4.4 KernelSU &>> $REALLOGGER
+	git clone https://github.com/riarumoda/KernelSU-4.4 &>> $REALLOGGER
 	cd drivers
 	ln -sf ../KernelSU/kernel kernelsu &>> $REALLOGGER
 	cd ..
